4678a4b8868703d5c1458c8db4a30c37e1ce2dc0
Skoryi Serhii <well.another.attempt@gmail.com>
2021-10-30 10:53:22 +0300

    spring initializr

---
+++ src/main/java/com/slowpoker/competition/CompetitionApplication.java
@@ -0,0 +1,13 @@
+package com.slowpoker.competition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetitionApplication.class, args);
+	}
+
+}
